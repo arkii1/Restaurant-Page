d2bd6d4a3162d354a880d43609b3c7b94c279e59
Tommy Mapp <tommy.mapp@hotmail.com>
2021-12-02 09:49:10 +0000

    Transfered html to be generated in js instead of hardcoded

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-log();
+import initHomeContent from './homepage'
+import initMenuContent from './menu'
 
-function log() {
-    console.log("Log");
+init();
+
+function init() {
+    console.log("init()");
+    initTemplate();
+    initHomeContent();
 }
+
+function initTemplate() {
+    console.log("initTemplate()");
+    const bgDiv = document.createElement('div');
+    bgDiv.classList.add("background-image");
+
+    const overlayDiv = document.createElement('div');
+    overlayDiv.classList.add("overlay");
+
+    bgDiv.appendChild(overlayDiv);
+    const content = document.getElementById('content');
+    content.appendChild(bgDiv);
+}
+
+
+
+
